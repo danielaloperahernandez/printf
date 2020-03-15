558e04a61744c30664c2a9decd88cd83173be70c
danielaloperahernandez <1469@holbertonschool.com>
2020-03-15 14:52:50 +0000

    Add modifications --printf.c Changes in loop (find_function) Changes in declaration of variables i and cprint (_printf function) Changes in conditional for find % (_printf function)

--- _printf.c
+++ _printf.c
@@ -15,46 +15,43 @@ int (*find_function(const char *format))(va_list)
 	while (find_f[i].sc)
 	{
 		if (find_f[i].sc[0] == *format)
-			return (find_f[i].f);
+			break;
+		i++;
 	}
+	return (find_f[i].f);
 }
 int _printf(const char *format, ...)
 {
 	va_list ap;
 	int (*f)(va_list);
-	int cprint = 0, int i = 0;
+	unsigned int i = 0, cprint = 0;
 
 	if (format == NULL)
 		return (-1);
 	va_start(ap, format);
 	while (format[i])
 	{
-		while (format[i] != %)
+		while (format[i] != '%' && format[i])
 		{
 			_putchar(format[i]);
-			cprint += 1;
+			cprint++;
 			i++;
 		}
 		if (format[i] == '\0')
 			return (cprint);
+		if (!format[i + 1])
+			return (-1);
 		f = find_function(format[i + 1]);
-		if (f == NULL)
-		{
-			if (format[i + 1] != "%");
-			{
-				return (-1);
-			}
-			else
-			{
-				_putchar('%');
-				cprint += 1;
-			}
-		}
-		else
+		if (f != NULL)
 		{
 			cprint += f(ap);
 			i += 2;
 		}
+		if (format[i + 1] == '%')
+			_putchar('%');
+			i += 2;
+		else 
+			i++;
 	}
 	va_end(ap);
 	return (cprint);

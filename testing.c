0e9d12c68d0cb71504bf7fa74e214cfc8c8e82ea
danielaloperahernandez <1469@holbertonschool.com>
2020-03-15 19:04:57 +0000

    Add file testing.c

---
+++ testing.c
@@ -0,0 +1,30 @@
+#include <limits.h>
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int len;
+   	int len2;
+	unsigned int ui;
+   	void *addr;
+
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
+	return (0);
+}

30613d132f3a5fdb694dd0d8a254e62928f9b0e6
danielaloperahernandez <1469@holbertonschool.com>
2020-03-15 01:09:44 +0000

    add print_string and print_dec and modify _putchar --holberton.h

--- holberton.h
+++ holberton.h
@@ -2,10 +2,12 @@
 #define HOLBERTONH
 
 #include <stdarg.h>
-int _putchar(char *c);
+int _putchar(char c);
 int _printf(const char *format, ...);
-int print_char(va_list);
-int print_int(va_list);
+int print_char(va_list c);
+int print_string(va_list s);
+int print_int(va_list i);
+int print_dec(va_list d);
 
 typedef struct code_format
 {

57c62d4d4442f4b8bdca106128d1cb0941c87e10
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-14 22:05:35 -0500

    Add files to print char and strings
    On branch test
    Your branch is up to date with 'origin/test'.
    
    Changes to be committed:
    	new file:   print_char.c
    	new file:   print_string.c

---
+++ print_char.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_char - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: 1.
+ */
+int print_char(va_list c)
+{
+        char my_char;
+
+        my_char = va_arg(c, int);
+        _putchar(my_char);
+        return (1);
+}
+/**
+ * print_porcentage - %
+ *
+ * Return: 1.
+ */
+int print_porcentage()
+{
+        _putchar('%');
+        return (1);
+}
---
+++ print_string.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_string - writes the character c to stdout
+ * @s: The string to print
+ *
+ * Return: 1.
+ */
+int print_string(va_list s)
+{
+        char *my_string;
+        int  i = 0;
+
+        my_string = va_arg(s, char *);
+        while (my_string[i])
+        {
+                _putchar(my_string);
+                i++;
+        }
+        return (i);
+}

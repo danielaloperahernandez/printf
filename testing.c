49e92ced7a9ee32e3825446342487ebe124959af
danielaloperahernandez <1469@holbertonschool.com>
2020-03-15 22:06:46 +0000

    Update test cases --testing.c

--- testing.c
+++ testing.c
@@ -11,6 +11,10 @@ int main(void)
 {
 	int len;
    	int len2;
+	int alen;
+	int elen;
+	char c = 'z';
+	/*char *str = NULL;*/
 	/*unsigned int ui;*/
    	/*void *addr;*/
 
@@ -26,5 +30,42 @@ int main(void)
 	printf("String:[%s]\n", "I am a string !");
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
+	printf("=====================\n");
+	printf("*****CHAR*****\n");
+	printf("=====================\n");
+	printf("Expected   : %c\n", 'a');
+	_printf("Actual     : %c\n", 'a');
+	elen = printf("Expected   : %c\n", c);
+	alen = _printf("Actual     : %c\n", c);
+	printf("Expected   : %i\n", elen);
+	printf("Actual     : %i\n", alen);
+	printf("Expected   : %cc\n", 'a');
+	_printf("Actual     : %cc\n", 'a');
+	printf("Expected   : %c\n", 53);
+	_printf("Actual     : %c\n", 53);
+	printf("Expected   : %c.\n", '\0');
+	_printf("Actual     : %c.\n", '\0');
+	printf("Expected   : %%%c\n", 'y');
+	_printf("Actual     : %%%c\n", 'y');
+	printf("=====================\n");
+	printf("*****STRINGS*****\n");
+	printf("=====================\n");
+	alen = elen = 0;
+	printf("Expected   : %s\n", "holberton");
+	_printf("Actual     : %s\n", "holberton");
+	printf("Expected   : %s$\n", "");
+	_printf("Actual     : %s$\n", "");
+	/*elen = printf("Expected   : %s\n", str);*/
+	/*alen = _printf("Actual     : %s\n", str);*/
+	/*elen = printf("%s", "");*/
+	/*alen = _printf("%s", "");*/
+	/*printf("Expected   : %i\n", elen);*/
+	/*printf("Actual     : %i\n", alen);*/
+	printf("Expected   : %sschool\n", "holberton");
+	_printf("Actual     : %sschool\n", "holberton");
+	/*elen = printf("Expected: %c%%%c%%%%%s%c%s%s\n", INT_MAX, INT_MIN, "hello", 0, "holberton", 0);*/
+	/*alen = _printf("Actual  : %c%%%c%%%%%s%c%s%s\n", INT_MAX, INT_MIN, "hello", 0, "holberton",0);*/
+	/*printf("Expected: %d\n", elen);*/
+	/*printf("Actual  : %d\n", alen);*/
 	return (0);
 }

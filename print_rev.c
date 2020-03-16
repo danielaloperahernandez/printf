99d2040147ca5963bfe141261d0380bac8df5363
danielaloperahernandez <1469@holbertonschool.com>
2020-03-16 21:19:35 +0000

    Changes to variable name --print_rev.c

--- print_rev.c
+++ print_rev.c
@@ -9,7 +9,7 @@
 int print_rev(va_list r)
 {
 	char *st;
-	int i, count = 0;
+	int i, j = 0;
 
 	st = va_arg(r, char *);
 	if (st == NULL)
@@ -19,7 +19,7 @@ int print_rev(va_list r)
 	for (i -= 1 ; i >= 0; i--)
 	{
 		_putchar(st[i]);
-		count++;
+		j++;
 	}
-	return (count);
+	return (j);
 }

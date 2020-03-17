5d736ae94d39c50e601f9798c8e17813235520c9
danielaloperahernandez <1469@holbertonschool.com>
2020-03-17 18:54:06 +0000

    Add archives for excersice 4 print_hex.c -- for x and X identifiers print_octal.c -- for o identifier print_unsig.c -- for u identifier Add structs --_printf.c Add prototypes --holberton.h

--- holberton.h
+++ holberton.h
@@ -9,6 +9,10 @@ int print_string(va_list s);
 int print_int(va_list i);
 int print_dec(va_list d);
 int print_rev(va_list r);
+int print_unsig(va_list u);
+int print_octal(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
 /**
   * struct code_format - Struct format
   *

238101ab28f0281f2eae0e35bf9f209e24ee515c
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-17 17:26:06 -0500

    Add function rot13.
    On branch test
    Your branch is up to date with 'origin/test'.
    
    Changes to be committed:
    	modified:   _printf.c
    	modified:   holberton.h
    	modified:   print_bin.c
    	new file:   print_rot13.c
    	modified:   test/testing.c
    
    Untracked files:
    	a.out

--- holberton.h
+++ holberton.h
@@ -14,6 +14,7 @@ int print_unsig(va_list u);
 int print_octal(va_list o);
 int print_x(va_list x);
 int print_X(va_list X);
+int print_rot13(va_list R);
 /**
   * struct code_format - Struct format
   *

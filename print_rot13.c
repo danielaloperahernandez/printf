ed79b188dbecc3ed665feb86793dce8e9d43426d
Miguel Palacios <oswaldo9605@gmail.com>
2020-03-17 18:20:59 -0500

    Fix betty style and string NULL--print_rot13.c

--- print_rot13.c
+++ print_rot13.c
@@ -2,17 +2,19 @@
 #include <stdio.h>
 /**
   * print_rot13 - encodes a string into rot13.
-  * @r: string to convert
-  * Return: s econdes
+  * @R: string to convert
+  * Return: size the output text
   */
 int print_rot13(va_list R)
 {
 	int j, i, count = 0;
 	char *r;
-
 	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 	r = va_arg(R, char *);
+	if (r == NULL)
+		r = "(null)";
 	for (j = 0; r[j] != '\0'; j++)
 	{
 		for (i = 0; input[i] != '\0'; i++)
